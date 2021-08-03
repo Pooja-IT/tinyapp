fabf73b1b9338df7a30d7dac4dc30cb875556b82
Pooja Patel <poojapatel.web@gmail.com>
2021-08-02 23:32:23 -0400

    add post method for url and create function for generate random numbers when we submit form

--- express_server.js
+++ express_server.js
@@ -2,6 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser"); //to make this data readable
+app.use(bodyParser.urlencoded({extended: true}));
+
+const getRandomString = (numOfChars) => {
+  let randomCharsStr = '';
+  const possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < numOfChars; i++) {
+    randomCharsStr += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+  }
+  return randomCharsStr;
+};
+//getRandomString(6);
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,6 +34,13 @@ app.listen(PORT, () => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase};
   res.render("urls_index", templateVars);
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send(getRandomString(6));
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 app.get("/urls/:shortURL", (req, res) => {  
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
