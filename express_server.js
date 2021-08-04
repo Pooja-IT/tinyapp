d240eefc38f151fd4a60156f87bb23bc2568d10b
Pooja Patel <poojapatel.web@gmail.com>
2021-08-03 21:39:14 -0400

    deleting existing urls from database

--- express_server.js
+++ express_server.js
@@ -39,6 +39,12 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
   //res.send(getRandomString(6)); 
 });
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect(`/urls`);
+});
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
